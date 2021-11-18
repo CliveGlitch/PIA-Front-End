3f3fb63942d4a83e0f36da5e02e223e493c2538c
Henry <enriqueachavez@hotmail.com>
2021-11-18 08:35:32 -0600

    Sign Up functionality 1

--- SignIn.php
+++ SignIn.php
@@ -1,29 +1,37 @@
 <?php
   include_once 'Header.php'
 ?>
-<div class="sign_in_form">
-  <form class="form">
+<section class="sign_in_form">
+  <form class="form" action="sign.inc.php" method="post">
       
       <h1 class="form_title">Sign Up</h1>
       <div class="form_container">
 
         <div class="input_form_container">
-        <input type="text" placeholder="Name">
+        <input type="text" name="name" placeholder="Full Name">
         </div>
           
         <div class="input_form_container">
-        <input type="text" placeholder="Email"> 
+        <input type="text" name="email" placeholder="Email"> 
+        </div>
+
+        <div class="input_form_container">
+        <input type="text" name="uid" placeholder="Username"> 
         </div>
           
         <div class="input_form_container">
-        <input type="password" placeholder="Password">
+        <input type="password" name="pwd" placeholder="Password">
+
+        <div class="input_form_container">
+        <input type="password" name="pwdrepeat" placeholder="Repear Password">
           
         </div>
-        <input type="submit" value="Login" class="form_button">
+        <input type="submit" name="submit" value="Login" class="form_button">
         <p class="form_text">Already have and account? <a class="link" href="LogIn.php">Log In </a></p>
       </div>
   </form>
-</div>
+</section>
+
 
 <?php
   include_once 'Footer.php'
