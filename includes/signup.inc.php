3fef087635a4629d843b78315d09a6a98f6ba27f
Henry <enriqueachavez@hotmail.com>
2021-11-18 14:54:20 -0600

    nothing

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -8,8 +8,7 @@ if (isset($_POST["submit"])){
     $pwd = $_POST["pwd"];
     $pwdrepeat = $_POST["pwdrepeat"];
 
-    require_once 'dbh.inc.php';
-    require_once 'functions.inc.php';
+    
 
 } else {
     header("location: ../SignUp.php");
