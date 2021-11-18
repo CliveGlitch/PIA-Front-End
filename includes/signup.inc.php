c58ab3e4b58f8b060ba339e586d5d9f1c9c70f39
Henry <enriqueachavez@hotmail.com>
2021-11-18 14:56:14 -0600

    functions no

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -9,7 +9,33 @@ if (isset($_POST["submit"])){
     $pwdrepeat = $_POST["pwdrepeat"];
 
     require_once 'dbh.inc.php';
-    
+
+    if(emptyInputSignup($name, $email, $username, $pwd, $pwdrepeat) !== false){
+        header("location: ../SignUp.php?error=emptyinput");
+        exit();
+    }
+
+    if(invalidUid($username) !== false){
+        header("location: ../SignUp.php?error=invaliduid");
+        exit();
+    }
+
+    if(invalidEmail($email) !== false){
+        header("location: ../SignUp.php?error=invalidemail");
+        exit();
+    }
+
+    if(pwdMatch($pwd, $pwdrepeat) !== false){
+        header("location: ../SignUp.php?error=pwdnotmatch");
+        exit();
+    }
+
+    if(uidExists($conn, $username, $email) !== false){
+        header("location: ../SignUp.php?error=usernametaken");
+        exit();
+    }
+
+    createUser($conn, $name, $email, $username, $pwd);
 
 } else {
     header("location: ../SignUp.php");
