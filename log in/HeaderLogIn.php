75ee562998866457319ef3ee6482636d028d9ca4
Henry <enriqueachavez@hotmail.com>
2021-11-18 15:56:36 -0600

    f

--- log in/HeaderLogIn.php
+++ log in/HeaderLogIn.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Ice Pick Lodge</title>
     <link rel = "icon" href = "../images/Icon.png" type = "image/x-icon">
-    <link rel="stylesheet" href="../styles.css" />
+    <link rel="stylesheet" href="styles.css" />
     <link rel="stylesheet" href="slider.css" />
     <link
       rel="stylesheet"
