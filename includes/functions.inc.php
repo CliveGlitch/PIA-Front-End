8c430e3cd9701a6819f15ab4ff3ce749d5a867e6
Henry <enriqueachavez@hotmail.com>
2021-11-18 14:50:33 -0600

    changes

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -10,7 +10,16 @@ function emptyInputSignup($name, $email, $username, $pwd, $pwdrepeat){
     return = $result;
 }
 
+function invalidUid($username){
+    $result;
+    if(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
+        $result = true;
+    } else {
+        $result = false;
+    }
 
+    return = $result;
+}
 
 function invalidEmail($email){
     $result;
