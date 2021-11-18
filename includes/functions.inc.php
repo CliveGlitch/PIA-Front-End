423baf492119576607e0361c1336cdd197143fb3
Henry <enriqueachavez@hotmail.com>
2021-11-18 14:46:17 -0600

    No user Id check

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -10,16 +10,7 @@ function emptyInputSignup($name, $email, $username, $pwd, $pwdrepeat){
     return = $result;
 }
 
-function invalidUid($username){
-    $result;
-    if(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
-        $result = true;
-    } else {
-        $result = false;
-    }
 
-    return = $result;
-}
 
 function invalidEmail($email){
     $result;
