80a8cbb9596e8c6752d87441530352283a74f622
Henry <enriqueachavez@hotmail.com>
2021-11-18 15:51:44 -0600

    Clean Up

--- log in/HeaderLogIn.php
+++ log in/HeaderLogIn.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Ice Pick Lodge</title>
-    <link rel = "icon" href = "Images/Icon.png" type = "image/x-icon">
+    <link rel = "icon" href = "../images/Icon.png" type = "image/x-icon">
     <link rel="stylesheet" href="../styles.css" />
     <link rel="stylesheet" href="../slider.css" />
     <link
