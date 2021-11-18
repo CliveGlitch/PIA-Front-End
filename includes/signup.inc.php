039f799fc74aeafb35da43c19d6c341b204459c9
Henry <enriqueachavez@hotmail.com>
2021-11-18 15:04:47 -0600

    corrections

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -8,8 +8,8 @@ if (isset($_POST["submit"])){
     $pwd = $_POST["pwd"];
     $pwdrepeat = $_POST["pwdrepeat"];
 
-    header("location: ../index.php")
-
+    header("location: ../index.php");
+    exit();
 
 } else {
     header("location: ../SignUp.php");
