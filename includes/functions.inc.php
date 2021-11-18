cbfb5500089fd97245939451bd913c7e9bfdc3e4
Henry <enriqueachavez@hotmail.com>
2021-11-18 15:12:21 -0600

    Nothing

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -1,12 +1,1 @@
 <?php
-function emptyInputSignup($name, $email, $username, $pwd, $pwdrepeat){
-    $result;
-    if(empty($name) || empty($email) || empty($username) || empty($pwd) || empty($pwdrepeat)){
-        $result = true;
-    } else {
-        $result = false;
-    }
-
-    return = $result;
-}
-
