f211cc58cefe60a8fa823fa0c1f35dd85fa7677c
Henry <enriqueachavez@hotmail.com>
2021-11-17 20:50:09 -0600

    Footer

--- LogIn.php
+++ LogIn.php
@@ -3,60 +3,6 @@
 ?>
 
     
-    <!-- Footer Section -->
-    <div class="footer__container">
-      <section class="social__media">
-        <div class="social__media--wrap">
-          <div class="footer__logo">
-            <a id="footer__logo">Ice-Pick Lodge</a>
-          </div>
-          <p class="website__rights">© Ice-Pick Lodge 2020. All rights reserved</p>
-          <div class="social__icons">
-            <a
-              class="social__icon--link"
-              href="https://www.facebook.com/icepicklodge"
-              target="_blank"
-              aria-label="Facebook"
-            >
-              <i class="fab fa-facebook"></i>
-            </a>
-            <a
-              class="social__icon--link"
-              href="https://www.instagram.com/icepicklodge/"
-              target="_blank"
-              aria-label="Instagram"
-            >
-              <i class="fab fa-instagram"></i>
-            </a>
-            <a
-              class="social__icon--link"
-              href="https://www.youtube.com/icepicklodge"
-              target="_blank"
-              aria-label="Youtube"
-            >
-              <i class="fab fa-youtube"></i>
-            </a>
-            <a
-              class="social__icon--link"
-              href="https://twitter.com/IcePickLodge"
-              target="_blank"
-              aria-label="Twitter"
-            >
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a
-              class="social__icon--link"
-              href="https://steamcommunity.com/groups/icepicklodge"
-              target="_blank"
-              aria-label="Steam"
-            >
-              <i class="fab fa-steam"></i>
-            </a>
-          </div>
-        </div>
-      </section>
-    </div>
-    <script src="app.js"></script>
-    <script src="jsfile.js"></script>
-  </body>
-</html>
+<?php
+  include_once 'Footer.php'
+?>
