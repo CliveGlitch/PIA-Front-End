be32ee167d75b603fab2e52b7fb720e8da96822f
Henry <enriqueachavez@hotmail.com>
2021-11-19 15:39:59 -0600

    DWADAEF

--- Header.php
+++ Header.php
@@ -29,10 +29,6 @@
     <nav class="navbar">
       <div class="navbar__container">
         <a href="index.php" id="navbar__logo"></i>Ice Pick Lodge</a>
-        <div class="navbar__toggle" id="mobile-menu">
-          <span class="bar"></span> <span class="bar"></span>
-          <span class="bar"></span>
-        </div>
         <ul class="navbar__menu">
           <li class="navbar__item">
             <a href="News.php" class="navbar__links">News</a>
