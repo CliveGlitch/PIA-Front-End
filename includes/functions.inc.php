9e240abb7b0563f9cd224975bebb738dc955b05f
Henry <enriqueachavez@hotmail.com>
2021-11-19 09:37:27 -0600

    cleanups

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -118,3 +118,24 @@ function logInUser($conn, $username, $pwd){
         exit();
     }
 }
+
+function getNumUsers($conn){
+    $sql = "SELECT COUNT(*) FROM users;";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../SignUp.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+    if($row = mysqli_fetch_assoc($resultData)){
+        return $resultData;
+    } else {
+        $result = false;
+        return $result;
+    }
+
+    mysqli_stmt_close($stmt);
+}
