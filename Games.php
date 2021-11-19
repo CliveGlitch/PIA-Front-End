719aa2c9f7eb60054ee5c242818da21f10743f9a
Henry <enriqueachavez@hotmail.com>
2021-11-19 09:52:27 -0600

    Game P2

--- Games.php
+++ Games.php
@@ -1,6 +1,29 @@
 <?php
   include_once 'Header.php'
 ?>
+  <div class="about-us-all">
+    <img class="about-us-img" src="images/S - Pathologic 2.png">
+    <div class="about-us-container">
+      <div class="about-us-text">
+      <span>Pathologic 2 is an open world survival thriller set in a town that’s being consumed by a deadly plague. Face the realities of a collapsing society as you make difficult choices in seemingly lose-lose situations. The plague isn’t just a disease. You can’t save everyone.<br><br>Get to know it, winning the affection of the locals and gaining allies, or try to carve your own path alone. Explore the Town, its inhabitants, and their traditions; fight both the plague itself and its victims; try to make a difference before the time runs out.<br><br>Figure out who killed your father while fighting a deadly plague. It’s up to you to assume his mantle of the town’s Chief Medic.<br><br>Find your alleged twin sister. Was she the one who had brought the plague to this town?<br>The local kids are hiding something. Try playing by their rules.<br><br>You only have 12 days.</span>
+      </div>
+      <div class="gallery">
+        <img src="images/P2.png" alt="Cinque Terre" width="600" height="400">
+      </div>
+
+      <div class="gallery">
+        <img src="images/P2 1.png" alt="Forest" width="600" height="400">
+      </div>
+
+      <div class="gallery">
+        <img src="images/P2 2.png" alt="Northern Lights" width="600" height="400">
+      </div>
+
+      <div class="gallery">
+        <img src="images/P2 3.png" alt="Mountains" width="600" height="400">
+      </div>
+    </div>
+  </div>
   <!-- Services Section -->
   <div class="gallery-container">
       <h1 class="gallery-all-title"> Check out our games! </h1>
