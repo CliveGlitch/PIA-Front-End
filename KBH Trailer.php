4831cb39627201fe286484c589541af1050a098a
Henry <enriqueachavez@hotmail.com>
2021-11-19 17:47:17 -0600

    fsfef

--- KBH Trailer.php
+++ KBH Trailer.php
@@ -12,20 +12,18 @@
           <br>Know by heart (First on PC/PS4/Xbox One/Nintendo Switch. Later on iPhone/Android)<br>
           </span>
           <a class="link_news" href="https://www.youtube.com/watch?v=PcqI9du-SJc">
-            <img class="about-us-img" src="images/Youtube KBH.jpg">
+            <img class="new-image-e" src="images/Youtube KBH.jpg">
           </a>
           <span><br><br>Know by Heart is a game about accepting loss. Players will be immersed into the quiet life of a provincial Russian town, where traces and echoes of the Soviet era can still be seen and heard.
           <br>A brief but tumultuous episode of the town’s life is interwoven with the story of a gang of childhood friends coming back together. It’s up to them to journey down memory lanes and rekindle their friendship, but their memories of the past and the very bonds between them become threatened...
           <br>Add "Know by heart" to your wishlist at Steam:
           </span>
-        </div>
-        <div class="about-us-container">
-          <li class='buy__btn'>
-            <a href='https://store.steampowered.com/app/685670/Know_by_heart/' class='buy_button'>Add to wishlist!</a>
-          </li>
-        </div>
+        </div> 
       </div>
-      <br>
+      <li class='buy__btn'>
+        <a href='https://store.steampowered.com/app/685670/Know_by_heart/' class='buy_button'>Add to wishlist!</a>
+      </li>
+      <span><br></span>
     </div>
     
 <?php
