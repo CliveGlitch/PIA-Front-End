01411686acd78a4355e76fa94d51ddb9803a5c7b
Henry <enriqueachavez@hotmail.com>
2021-11-19 09:21:18 -0600

    aaa

--- Header.php
+++ Header.php
@@ -9,7 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Ice Pick Lodge</title>
     <link rel = "icon" href = "images/Icon.png" type = "image/x-icon">
-    
+    <link rel="stylesheet" href="assets/styles.css" />
+    <link rel="stylesheet" href="assets/slider.css" />
     <link
       rel="stylesheet"
       href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
@@ -20,17 +21,7 @@
       href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
       rel="stylesheet"
     />
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <!-- Google fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Muli:300,700&display=swap" rel="stylesheet">
-
-    <!-- Ionic icons -->
-    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/styles.css" />
-    <link rel="stylesheet" href="assets/slider.css" />
+    
   </head>
   <body>
     <!-- Navbar Section -->
