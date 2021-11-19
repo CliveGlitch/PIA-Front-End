d687067c88dab58bd19a24c3c468fb8bf975b0bd
Henry <enriqueachavez@hotmail.com>
2021-11-19 10:15:09 -0600

    asad

--- Games.php
+++ Games.php
@@ -6,8 +6,8 @@
     <div class="about-us-container">
       <div class="about-us-text">
       <span>Pathologic 2 is an open world survival thriller set in a town that’s being consumed by a deadly plague. Face the realities of a collapsing society as you make difficult choices in seemingly lose-lose situations. The plague isn’t just a disease. You can’t save everyone.<br><br>Get to know it, winning the affection of the locals and gaining allies, or try to carve your own path alone. Explore the Town, its inhabitants, and their traditions; fight both the plague itself and its victims; try to make a difference before the time runs out.<br><br>Figure out who killed your father while fighting a deadly plague. It’s up to you to assume his mantle of the town’s Chief Medic.<br><br>Find your alleged twin sister. Was she the one who had brought the plague to this town?<br>The local kids are hiding something. Try playing by their rules.<br><br>You only have 12 days.</span>
-      <span class="gallery-all-title">Features:</span>
-      <span> - 12 days in an odd town ravaged by a deadly disease.<br> - Time is of the essence: if you don’t manage it carefully, it’ll simply run out. You’ll have to choose how to spend the priceless minutes you have.<br> - Survival thriller. You’ll have to manage your bodily functions, offsetting hunger, thirst, exhaustion, and so on. But it doesn’t boil down to scavenging resources. Surviving on your own is hard; you’ll have to win over allies.<br> - An uphill battle. Managing your bodily parameters may seem bearable at first, but as time goes by, it becomes harder and harder. Your own body is only waiting for an opportunity to give up and betray you.  Things are changing from bad to worse and the odds are stacked against you.<br> - A duel with an enemy you can’t kill. Your main foe is the plague itself, an incorporeal and malevolent entity that you have to defeat… without having the means to. It’s more powerful and more treacherous than you can imagine.<br> - Loot, murder, mug, steal, barter, beg… or don’t. You need resources to survive, but it’s up to you how to obtain them.<br> - The fights are short, ungraceful, and vicious. They don’t have to be lethal though. Many people—yourself included—would prefer to exchange their wallet for their life.</span>
+      <span class="gallery-all-title"><br>Features:</span>
+      <span><br> - 12 days in an odd town ravaged by a deadly disease.<br> - Time is of the essence: if you don’t manage it carefully, it’ll simply run out. You’ll have to choose how to spend the priceless minutes you have.<br> - Survival thriller. You’ll have to manage your bodily functions, offsetting hunger, thirst, exhaustion, and so on. But it doesn’t boil down to scavenging resources. Surviving on your own is hard; you’ll have to win over allies.<br> - An uphill battle. Managing your bodily parameters may seem bearable at first, but as time goes by, it becomes harder and harder. Your own body is only waiting for an opportunity to give up and betray you.  Things are changing from bad to worse and the odds are stacked against you.<br> - A duel with an enemy you can’t kill. Your main foe is the plague itself, an incorporeal and malevolent entity that you have to defeat… without having the means to. It’s more powerful and more treacherous than you can imagine.<br> - Loot, murder, mug, steal, barter, beg… or don’t. You need resources to survive, but it’s up to you how to obtain them.<br> - The fights are short, ungraceful, and vicious. They don’t have to be lethal though. Many people—yourself included—would prefer to exchange their wallet for their life.</span>
       </div>
 
       <div class="gallery-mini">
@@ -16,32 +16,34 @@
         <img class="gallery-mini-img" src="images/P2 2.jpg">
         <img class="gallery-mini-img" src="images/P2 3.jpg">
       </div>
+
+      <!-- Services Section -->
+      <div class="gallery-container">
+        <h1 class="gallery-all-title"> Check out our games! </h1>
+        <div class="gallery-all">
+          <div class="gallery">
+            <img class="img-gallery" src="images/S - Pathologic 2.png" alt="">
+          </div>
+          <div class="gallery">
+            <img class="img-gallery" src="images/S - Know by Heart.png" alt="">
+          </div>
+          <div class="gallery">
+            <img class="img-gallery" src="images/S - Cargo.png" alt="">
+          </div>
+          <div class="gallery">
+            <img class="img-gallery" src="images/S - Knock Knock.png" alt="">
+          </div>
+          <div class="gallery">
+            <img class="img-gallery" src="images/S - Pathologic.png" alt="">
+          </div>
+          <div class="gallery">
+            <img class="img-gallery" src="images/S - The Void.png" alt="">
+          </div>
+        </div>
+    </div>
       
     </div>
-    <!-- Services Section -->
-    <div class="gallery-container">
-      <h1 class="gallery-all-title"> Check out our games! </h1>
-      <div class="gallery-all">
-        <div class="gallery">
-          <img class="img-gallery" src="images/S - Pathologic 2.png" alt="">
-        </div>
-        <div class="gallery">
-          <img class="img-gallery" src="images/S - Know by Heart.png" alt="">
-        </div>
-        <div class="gallery">
-          <img class="img-gallery" src="images/S - Cargo.png" alt="">
-        </div>
-        <div class="gallery">
-          <img class="img-gallery" src="images/S - Knock Knock.png" alt="">
-        </div>
-        <div class="gallery">
-          <img class="img-gallery" src="images/S - Pathologic.png" alt="">
-        </div>
-        <div class="gallery">
-          <img class="img-gallery" src="images/S - The Void.png" alt="">
-        </div>
-      </div>
-    </div>
+    
   </div>
   
 <?php
