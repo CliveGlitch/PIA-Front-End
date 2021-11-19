883663ebfa2f800ff586f82463fdd37329bfdc94
Henry <enriqueachavez@hotmail.com>
2021-11-19 14:14:09 -0600

    column news

--- News.php
+++ News.php
@@ -6,11 +6,15 @@
   <div class="about-us-container">
 
   <div class="card_news">
-    <img src="images/Game pass.jpg" class="card_news__image" alt="brown couch" />
-    <div class="card_news__content">
-      <time datetime="2021-03-30" class="card_news__date">30 März 2021</time>
-      <span class="card_news__title">Duis autem vel eum iriure dolor in hend in vulputate<span>
-    </div>
+    <div class="row">
+      <div class="column"></div>
+        <img src="images/Game pass.jpg" class="card_news__image" alt="brown couch" />
+      <div class="column"></div>
+        <div class="card_news__content">
+          <time datetime="2021-03-30" class="card_news__date">30 März 2021</time>
+          <span class="card_news__title">Duis autem vel eum iriure dolor in hend in vulputate<span>
+        </div>
+    </div>   
   </div>
 
   </div>
