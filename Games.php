9d13ba1a0dd31ad6f0c8b128b3fea1fa20b2c9f0
Henry <enriqueachavez@hotmail.com>
2021-11-19 10:31:09 -0600

    button buy

--- Games.php
+++ Games.php
@@ -4,6 +4,9 @@
   <div class="about-us-all">
     <img class="about-us-img" src="images/Pathologic2.jpg">
     <div class="about-us-container">
+      <li class='navbar__btn'>
+        <a href='https://store.steampowered.com/app/505230/Pathologic_2/' class='button'>Buy on Steam!</a>
+      </li>
       <div class="about-us-text">
       <span>Pathologic 2 is an open world survival thriller set in a town that’s being consumed by a deadly plague. Face the realities of a collapsing society as you make difficult choices in seemingly lose-lose situations. The plague isn’t just a disease. You can’t save everyone.<br><br>Get to know it, winning the affection of the locals and gaining allies, or try to carve your own path alone. Explore the Town, its inhabitants, and their traditions; fight both the plague itself and its victims; try to make a difference before the time runs out.<br><br>Figure out who killed your father while fighting a deadly plague. It’s up to you to assume his mantle of the town’s Chief Medic.<br><br>Find your alleged twin sister. Was she the one who had brought the plague to this town?<br>The local kids are hiding something. Try playing by their rules.<br><br>You only have 12 days.</span>
       <span class="gallery-all-title"><br><br>Features:</span>
@@ -29,7 +32,7 @@
 
       <!-- Services Section -->
       <div class="gallery-container">
-        <h1 class="gallery-all-title"> Check out our games! </h1>
+        <h1 class="gallery-all-title"> Our games </h1>
         <div class="gallery-all">
           <div class="gallery">
             <img class="img-gallery" src="images/S - Pathologic 2.png" alt="">
