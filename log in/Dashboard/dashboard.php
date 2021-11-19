978aaa09387aeabbb46e93f28fbcc3d48cac9144
Henry <enriqueachavez@hotmail.com>
2021-11-18 18:22:41 -0600

    Dashboard

--- log in/Dashboard/dashboard.php
+++ log in/Dashboard/dashboard.php
@@ -77,11 +77,11 @@
                   <div class="container">
                       <div class="row">
                           <div class="col-lg-9 col-md-8">
-                            <h1 class="font-weight-bold mb-0">Bienvenido Diego</h1>
-                            <p class="lead text-muted">Revisa la última información</p>
+                            <h1 class="font-weight-bold mb-0">Welcome Admin</h1>
+                            <p class="lead text-muted">Check the latest report</p>
                           </div>
                           <div class="col-lg-3 col-md-4 d-flex">
-                            <button class="btn btn-primary w-100 align-self-center">Descargar reporte</button>
+                            <button class="btn btn-primary w-100 align-self-center">Download Report</button>
                           </div>
                       </div>
                   </div>
@@ -94,30 +94,30 @@
                             <div class="row">
                                 <div class="col-lg-3 col-md-6 d-flex stat my-3">
                                     <div class="mx-auto">
-                                        <h6 class="text-muted">Ingresos mensuales</h6>
-                                        <h3 class="font-weight-bold">$50000</h3>
+                                        <h6 class="text-muted">Game Revenue (Steam)</h6>
+                                        <h3 class="font-weight-bold">$</h3>
                                         <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 50.50%</h6>
                                     </div>
                                 </div>
                                 <div class="col-lg-3 col-md-6 d-flex stat my-3">
                                     <div class="mx-auto">
-                                        <h6 class="text-muted">Productos activos</h6>
-                                        <h3 class="font-weight-bold">100</h3>
-                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 25.50%</h6>
+                                        <h6 class="text-muted">Active Players (Steam)</h6>
+                                        <h3 class="font-weight-bold">5342</h3>
+                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 12.50%</h6>
                                     </div>
                                 </div>
                                 <div class="col-lg-3 col-md-6 d-flex stat my-3">
                                     <div class="mx-auto">
-                                        <h6 class="text-muted">No. de usuarios</h6>
-                                        <h3 class="font-weight-bold">2500</h3>
-                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 75.50%</h6>
+                                        <h6 class="text-muted">Number of Users</h6>
+                                        <h3 class="font-weight-bold">1200</h3>
+                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 81.50%</h6>
                                     </div>
                                 </div>
                                 <div class="col-lg-3 col-md-6 d-flex my-3">
                                     <div class="mx-auto">
-                                        <h6 class="text-muted">Usuarios nuevos</h6>
-                                        <h3 class="font-weight-bold">500</h3>
-                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 15.50%</h6>
+                                        <h6 class="text-muted">New Users</h6>
+                                        <h3 class="font-weight-bold">124</h3>
+                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 23.00%</h6>
                                     </div>
                                 </div>
                             </div>
@@ -132,7 +132,7 @@
                           <div class="col-lg-8 my-3">
                               <div class="card rounded-0">
                                   <div class="card-header bg-light">
-                                    <h6 class="font-weight-bold mb-0">Número de usuarios de paga</h6>
+                                    <h6 class="font-weight-bold mb-0">Number of users</h6>
                                   </div>
                                   <div class="card-body">
                                     <canvas id="myChart" width="300" height="150"></canvas>
@@ -142,7 +142,7 @@
                           <div class="col-lg-4 my-3">
                             <div class="card rounded-0">
                                 <div class="card-header bg-light">
-                                    <h6 class="font-weight-bold mb-0">Ventas recientes</h6>
+                                    <h6 class="font-weight-bold mb-0">PC Games Sales</h6>
                                 </div>
                                 <div class="card-body pt-2">
                                     <div class="d-flex border-bottom py-2">
@@ -150,8 +150,8 @@
                                           <h2 class="align-self-center mb-0"><i class="icon ion-md-pricetag"></i></h2>
                                         </div>
                                         <div class="align-self-center">
-                                          <h6 class="d-inline-block mb-0">$250</h6><span class="badge badge-success ml-2">10% descuento</span>
-                                          <small class="d-block text-muted">Curso diseño web</small>
+                                          <h6 class="d-inline-block mb-0">$15</h6><span class="badge badge-success ml-2">-50%</span>
+                                          <small class="d-block text-muted">Pathologic 2 Steam</small>
                                         </div>
                                     </div>
                                     <div class="d-flex border-bottom py-2">
@@ -159,8 +159,8 @@
                                           <h2 class="align-self-center mb-0"><i class="icon ion-md-pricetag"></i></h2>
                                         </div>
                                         <div class="align-self-center">
-                                          <h6 class="d-inline-block mb-0">$250</h6><span class="badge badge-success ml-2">10% descuento</span>
-                                          <small class="d-block text-muted">Curso diseño web</small>
+                                          <h6 class="d-inline-block mb-0">$24</h6><span class="badge badge-success ml-2">-20%</span>
+                                          <small class="d-block text-muted">Pathologic 2 GOG</small>
                                         </div>
                                     </div>
                                     <div class="d-flex border-bottom py-2">
@@ -168,8 +168,8 @@
                                           <h2 class="align-self-center mb-0"><i class="icon ion-md-pricetag"></i></h2>
                                         </div>
                                         <div class="align-self-center">
-                                          <h6 class="d-inline-block mb-0">$250</h6><span class="badge badge-success ml-2">10% descuento</span>
-                                          <small class="d-block text-muted">Curso diseño web</small>
+                                          <h6 class="d-inline-block mb-0">$2</h6><span class="badge badge-success ml-2">-80%</span>
+                                          <small class="d-block text-muted">Pathologic Steam</small>
                                         </div>
                                     </div>
                                     <div class="d-flex border-bottom py-2">
@@ -177,8 +177,8 @@
                                           <h2 class="align-self-center mb-0"><i class="icon ion-md-pricetag"></i></h2>
                                         </div>
                                         <div class="align-self-center">
-                                          <h6 class="d-inline-block mb-0">$250</h6><span class="badge badge-success ml-2">10% descuento</span>
-                                          <small class="d-block text-muted">Curso diseño web</small>
+                                          <h6 class="d-inline-block mb-0">$5</h6><span class="badge badge-success ml-2">-50%</span>
+                                          <small class="d-block text-muted">Pathologic GOG</small>
                                         </div>
                                     </div>
                                     <div class="d-flex border-bottom py-2 mb-3">
@@ -186,11 +186,11 @@
                                           <h2 class="align-self-center mb-0"><i class="icon ion-md-pricetag"></i></h2>
                                         </div>
                                         <div class="align-self-center">
-                                          <h6 class="d-inline-block mb-0">$250</h6><span class="badge badge-success ml-2">10% descuento</span>
-                                          <small class="d-block text-muted">Curso diseño web</small>
+                                          <h6 class="d-inline-block mb-0">$15</h6><span class="badge badge-success ml-2">-50%</span>
+                                          <small class="d-block text-muted">Pathologic 2 Steam</small>
                                         </div>
                                     </div>
-                                    <button class="btn btn-primary w-100">Ver todas</button>
+                                    <button class="btn btn-primary w-100">See all</button>
                                 </div>
                             </div>
                           </div>
@@ -220,9 +220,9 @@
             var myChart = new Chart(ctx, { 
                 type: 'bar',
                 data: {
-                    labels: ['Feb 2020', 'Mar 2020', 'Abr 2020', 'May 2020'],
+                    labels: ['Agt 2021', 'Sep 2021', 'Oct 2021', 'Nov 2021'],
                     datasets: [{
-                        label: 'Nuevos usuarios',
+                        label: 'New Users',
                         data: [50, 100, 150, 200],
                         backgroundColor: [
                             '#12C9E5',  
@@ -230,7 +230,7 @@
                             '#12C9E5',
                             '#111B54'
                         ],
-                        maxBarThickness: 30,
+                        maxBarThickness: 75,
                         maxBarLength: 2
                     }]
                 },
