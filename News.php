3a10ad67204ab4c162d59cc59394a2f05c1ce0ed
Henry <enriqueachavez@hotmail.com>
2021-11-19 14:56:21 -0600

    News blog complete

--- News.php
+++ News.php
@@ -6,11 +6,29 @@
   <div class="about-us-container">
 
   <div class="card_news">
-    <img src="images/Game pass.jpg" class="card_news__image" alt="brown couch" />
+    <img src="images/Know by heart.jpg" class="card_news__image"/>
+    <div class="card_news__content">
+      <span class="card_news__title">New Trailer of Know by heart out!</span>
+      <span class="card_news__text">New Trailer of Know by heart out!<br>Know by heart (First on PC/PS4/Xbox One/Nintendo Switch. Later on iPhone/Android)<span>
+      <span class="card_news__date">December 31, 2020</span>
+    </div>
+  </div>
+  
+  <div class="card_news">
+    <img src="images/Game pass.jpg" class="card_news__image"/>
     <div class="card_news__content">
       <span class="card_news__title">Pathologic 2 available in Game Pass</span>
       <span class="card_news__text">Pathologic 2 will be available on Xbox Game Pass.<br>If you follow Xbox news closely, you probably know what it is already: a service that provides the subscriber a number of games for a single subscription fee.<span>
       <span class="card_news__date">November 13, 2018</span>
+    </div>
+  </div>
+
+  <div class="card_news">
+    <img src="images/the marbel nest.jpg" class="card_news__image"/>
+    <div class="card_news__content">
+      <span class="card_news__title">The Marble Nest is Here!</span>
+      <span class="card_news__text">Pathologic 2: The Marble Nest DLC, a two-hour spinoff story in the world of the Pathologic 2.<br>Play for a single day as Dr. Daniil Dankovsky, a scientist from the Capital, a fighter of death, and another doctor trying to...<span>
+      <span class="card_news__date">October 28, 2019</span>
     </div>
   </div>
 
